acd6b20da48183d4e9c35958a528d804b032105c
jovanac99 <comjovana99@gmail.com>
2022-02-05 21:46:40 +0100

    Login.js implementirana, forma, kontrolisanje input polja, useState primena

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,31 @@
+import { useState } from 'react'
 
 function Login(props) {
-    return (
-        <div className="poc-div">
 
+    const allUsers = props.users
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleUsername(e) {
+        setUsername(e.target.value)
+    }
+
+    function handlePassword(e) {
+        setPassword(e.target.value)
+    }
+
+    return (
+        <div className="login-div">
+            <h1>Login form</h1>
+            <div className="login-gr" id='usr-lg'>
+                <label>Username: </label>
+                <input type="text" className="form-control mt-2 mb-2" value={username} onChange={handleUsername} />
+            </div>
+            <div className="login-gr">
+                <label>Password: </label>
+                <input type="password" className="form-control mt-2 mb-2" value={password} onChange={handlePassword} />
+            </div>
+            <button type="button" id='dugme' className='btn btn-primary btn-lg mt-3'>Login</button>
         </div>
     );
 }
